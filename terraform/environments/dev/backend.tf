8b0b3a2d041a84c8cbb7cb2d45d05cddd1a365c8
seed111 <olusheyetech@gmail.com>
2026-06-04 09:16:21 +0200

    trigger: initial deploy

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "ecommerce-terraform-state-dev"
+    bucket         = "ecommerce-terraform-state-dev-seed111"
     key            = "dev/terraform.tfstate"
     region         = "eu-west-1"
     dynamodb_table = "ecommerce-terraform-locks"
